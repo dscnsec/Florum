3519f805062d2addfecb93ed78e7f05a6cf4fa31
root <root@localhost.localdomain>
2019-10-16 00:17:37 +0530

    sharad

--- storage/views/2e259fbea31579f9fb814516fdfb8275585410fd.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('content'); ?>
-  <p>
-    <?php echo e($message); ?>
-
-  </p>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('flarum.forum::layouts.basic', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- vendor/flarum/mentions/views/emails/userMentioned.blade.php
+++
@@ -1,9 +0,0 @@
-Hey {!! $user->display_name !!}!
-
-{!! $blueprint->post->user->display_name !!} mentioned you in a post in {!! $blueprint->post->discussion->title !!}.
-
-{!! app()->url() !!}/d/{!! $blueprint->post->discussion_id !!}/{!! $blueprint->post->number !!}
-
----
-
-{!! $blueprint->post->content !!}
