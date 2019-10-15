a25c8b24a2164f743f9d4d49e3790cde610d1ab8
Ubuntu <password@flarumvm.zoo01xljoabulg3ka5px1znr5f.bx.internal.cloudapp.net>
2019-10-15 18:11:34 +0000

    New Init

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'florum',
+    'username' => 'florumuser',
+    'password' => 'florumpass',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'florumpass',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'https://florum-test.dscnsec.com',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
